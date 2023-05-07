25947cb066e400e5664f3f1e1cfb3004405267b9
Crowdbotics <engineering@crowdbotics.com>
2023-05-07 18:01:18 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,392 +1,404 @@
-import React, { useState, useEffect } from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  Image,
-  Pressable,
-  TextInput
-} from "react-native";
+import React from "react";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 
-const VideoCallDetails = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
-  const [meetingDetails, setMeetingDetails] = useState({});
-  useEffect(() => {
-    setMeetingDetails({
-      title: "Sales Strategy Session",
-      pin: 45620,
-      url: "https://crowdbotics/abc.test-123"
-    });
-  }, []);
+const walletHomeScreen = () => {
   return (
     <View style={styles.container}>
+      <ScrollView>
       <View style={styles.header}>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/videoIcon.png")}
-            style={styles.icon}
-          />
+        <View style={styles.headerIcons}>
+            <Image source={require(
+              // @ts-ignore
+              "./assets/back.png")} />
+             <Image source={require(
+               // @ts-ignore
+               "./assets/bell.png")} />
         </View>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/voiceIcon.png")}
-            style={styles.icon}
-          />
+        <View>
+          <Text style={styles.availText}>Available</Text>
+          <Text style={styles.coinsText}>Coins/Cash</Text>
         </View>
-        <View style={styles.roundIconContainer}>
-          <Image
-            source={require("./assets/phoneIcon.png")}
-            style={styles.icon}
-          />
+        <View style={styles.amounts}>
+          <Text style={styles.getAmount}>$1,893.90 <Text style={styles.totalAmount}>/ $5,250.35</Text></Text>
         </View>
-        <View style={[styles.roundIconContainer, styles.redBg]}>
-          <Image
-            source={require("./assets/callIcon.png")}
-            style={styles.icon}
-          />
+        <View style={styles.slider}>
+          <Image style={styles.sliderImg} source={require(
+            // @ts-ignore
+            "./assets/slider.png")} />
+        </View>
+        <View style={styles.tabSection}>
+            <View>
+              <Text style={styles.heading}>Coin</Text>
+              <Text style={styles.subHeading}>$232.99</Text>
+            </View>
+            <View style={styles.border}></View>
+            <View>
+              <Text style={styles.heading}>Cash</Text>
+              <Text style={styles.subHeading}>$432.90</Text>
+            </View>
+            <View style={styles.border}></View>
+            <View>
+              <Text style={styles.heading}>Staked Coins</Text>
+              <Text style={styles.subHeading}>$150.00</Text>
+            </View>
         </View>
       </View>
-      <TabView
-        tabTitles={["People", "Details"]}
-        selected={selectedTab}
-        onPress={setSelectedTab}
-        style={styles.tabView}
-      />
-      <View style={styles.separator}>
-        <Text style={styles.separatorText}>{meetingDetails.title}</Text>
+      <View style={styles.tabView}>
+        <View style={styles.tabItem}>
+          <Text style={styles.tabText}>Exchange</Text>
+        </View>
+        <View style={styles.tabItem}>
+          <Text style={styles.tabText}>Pay</Text>
+        </View>
+        <View style={styles.tabItem}>
+          <Text style={styles.tabText}>Deposit</Text>
+        </View>
       </View>
-      <Input
-        value={meetingDetails.url}
-        editable={false}
-        icon={require("./assets/linkIcon.png")}
-        containerStyle={styles.input}
-      />
-      <Input
-        value={"PIN: " + meetingDetails.pin}
-        editable={false}
-        icon={require("./assets/pinIcon.png")}
-        containerStyle={styles.input}
-      />
-      <Button buttonText="Share Joining Info" style={styles.button}>
-        <Image
-          source={require("./assets/shareIcon.png")}
-          style={styles.btnIcon}
-        />
-      </Button>
+      <View style={styles.walletHeadings}>
+            <Text style={styles.mainHeading}>List of wallets</Text>
+            <Text style={styles.mainHeading}>Balance</Text>
+      </View>
+      <View style={styles.mainContainer}>
+        <View style={styles.innerView}>
+            <View style={styles.innerHalfView}>
+              <Image style={styles.sliderImg} source={require(
+                // @ts-ignore
+                "./assets/ethereum.png")} />
+            <View>
+              <Text style={styles.coinText}>Ethereum</Text>
+              <Text style={styles.coinSubText}>ETH</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.balanceText}>$1.893,04</Text>
+            <Text style={styles.balanceAmountTextPositive}>+%11.89</Text>
+          </View>
+        </View>
+        <View style={styles.borderWhite}></View>
+
+        <View style={styles.innerView}>
+            <View style={styles.innerHalfView}>
+              <Image style={styles.sliderImg} source={require(
+                // @ts-ignore
+                "./assets/bitcoin.png")} />
+            <View>
+              <Text style={styles.coinText}>Bitcoin</Text>
+              <Text style={styles.coinSubText}>BTC</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.balanceText}>$37.818,60</Text>
+            <Text style={styles.balanceAmountTextNegative}>-%9.89</Text>
+          </View>
+        </View>
+        <View style={styles.borderWhite}></View>
+
+        <View style={styles.innerView}>
+            <View style={styles.innerHalfView}>
+              <Image style={styles.sliderImg} source={require(
+                // @ts-ignore
+                "./assets/ripple.png")} />
+            <View>
+              <Text style={styles.coinText}>Ripple</Text>
+              <Text style={styles.coinSubText}>XRP</Text>
+            </View>
+          </View>
+          <View>
+            <Text style={styles.balanceText}>$142,12</Text>
+            <Text style={styles.balanceAmountTextPositive}>+%1.89</Text>
+          </View>
+        </View>
+      </View>
+        <View style={styles.walletAreaSection}>
+            <Text style={styles.mainHeading}>Currency chat</Text>
+            <Image style={styles.sliderImg} source={require(
+              // @ts-ignore
+              "./assets/dropdown.png")} />
+        </View>
+        <View style={styles.imageGraph}>
+          <Image style={styles.graphImg} source={require(
+            // @ts-ignore
+            "./assets/card.png")} />
+        </View>
+
+        <View style={styles.indicatorsSection}>
+            <View style={styles.innerIndicator}>
+              <View style={styles.blueCircle}></View>
+              <Text style={styles.subHeadingTwo}>Ethereum</Text>
+            </View>
+            <View style={styles.border}></View>
+            <View style={styles.innerIndicator}>
+              <Text style={styles.greenCircle}></Text>
+              <Text style={styles.subHeadingTwo}>Bitcoin</Text>
+            </View>
+            <View style={styles.border}></View>
+            <View style={styles.innerIndicator}>
+              <Text style={styles.redCircle}></Text>
+              <Text style={styles.subHeadingTwo}>Ripple</Text>
+            </View>
+        </View>
+      <View style={styles.tabParent}>
+        <View style={styles.tabViewTwo}>
+          <View style={styles.tabItem}>
+            <Text style={styles.tabText}>Buy</Text>
+          </View>
+          <View style={styles.tabItem}>
+            <Text style={styles.tabText}>Sell</Text>
+          </View>
+          <View style={styles.tabItem}>
+            <Text style={styles.tabText}>Swap</Text>
+          </View>
+          <View style={styles.tabItem}>
+            <Text style={styles.tabText}>History</Text>
+          </View>
+        </View>
+      </View>
+      </ScrollView>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
+
   container: {
-    flex: 1,
-    backgroundColor: "#fff"
-  },
-  heading: {
-    fontSize: 20
+    backgroundColor: "white",
+    display: "flex",
+    paddingVertical: 20,
+    height: "100%"
   },
   header: {
-    justifyContent: "space-around",
-    alignItems: "flex-end",
-    backgroundColor: "#FCF1D6",
-    height: 180,
+    height: 237,
+    backgroundColor: "#F1F1F1",
+    paddingHorizontal: 25,
+    paddingVertical: 25
+  },
+  headerIcons: {
+    display: "flex",
     flexDirection: "row",
-    paddingHorizontal: 30,
-    paddingVertical: 20
+    justifyContent: "space-between"
   },
-  roundIconContainer: {
-    width: 50,
-    height: 50,
-    backgroundColor: "#fff",
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center"
+  availText: {
+    fontSize: 14,
+    color: "#111112",
+    marginTop: 15
   },
-  icon: {
-    width: 30,
-    height: 30,
-    resizeMode: "contain"
+  coinsText: {
+    fontSize: 11,
+    color: "#7C7C7C"
   },
-  redBg: {
-    backgroundColor: "#EA4335"
+  getAmount: {
+    fontSize: 26,
+    color: "#12D790",
+    fontWeight: "500"
+  },
+  totalAmount: {
+    fontSize: 12,
+    color: "#000000",
+    fontWeight: "500"
+  },
+  amounts: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 10
+  },
+  slider: {
+    marginTop: 14
+  },
+  sliderImg: {
+    borderRadius: 5
+  },
+  tabSection: {
+    height: 77,
+    backgroundColor: "#FFFFFF",
+    borderRadius: 10,
+    marginTop: 24,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingHorizontal: 10
+  },
+  heading: {
+    fontSize: 14,
+    color: "#26292A",
+    fontWeight: "500",
+    textAlign: "center"
+  },
+  subHeading: {
+    fontSize: 16,
+    color: "#12D790",
+    fontWeight: "500",
+    textAlign: "center"
+  },
+  subHeadingTwo: {
+    fontSize: 14,
+    color: "#26292A",
+    fontWeight: "500",
+    marginLeft: 10
+  },
+  border: {
+    borderWidth: 1,
+    height: 49,
+    borderColor: "#F2F2F2"
   },
   tabView: {
-    marginVertical: 20,
-    marginHorizontal: 20,
-    width: "70%"
-  },
-  separator: {
-    height: 50,
-    backgroundColor: "#DADADA",
-    justifyContent: "center",
-    paddingHorizontal: 20,
-    marginBottom: 20
-  },
-  separatorText: {
-    fontSize: 16
-  },
-  input: {
-    marginHorizontal: 20
-  },
-  button: {
-    marginHorizontal: 20,
-    marginVertical: 20
-  },
-  btnIcon: {
-    width: 20,
-    height: 20,
-    resizeMode: "contain",
-    marginLeft: 10
-  }
-});
-
-export default VideoCallDetails;
-
-const TabView = ({
-  tabTitles,
-  selected,
-  onPress,
-  tabColor,
-  backgroundColor,
-  style,
-  icons
-}) => {
-  const tabColorStyle = {
-    backgroundColor: tabColor || "#fff"
-  };
-  const backgroundColorStyle = {
-    backgroundColor: backgroundColor || "#F1F1F1"
-  };
-  const propStyle = style || {};
-  return (
-    <View
-      style={[tabViewStyles.paletteContainer, backgroundColorStyle, propStyle]}>
-      {tabTitles.map((title, index) => (
-        <Pressable
-          onPress={() => (onPress ? onPress(index) : null)}
-          style={
-            index === selected
-              ? [tabViewStyles.selected, tabColorStyle, tabViewStyles.tabItem]
-              : [
-                  tabViewStyles.unSelected,
-                  backgroundColorStyle,
-                  tabViewStyles.tabItem
-                ]
-          }
-          key={index}>
-          {icons
-            ? (
-            <Image
-              source={icons[index]}
-              style={[
-                tabViewStyles.icon,
-                index === selected
-                  ? tabViewStyles.selectedIcon
-                  : tabViewStyles.unSelectedIcon
-              ]}
-            />
-              )
-            : null}
-          <Text>{title}</Text>
-        </Pressable>
-      ))}
-    </View>
-  );
-};
-
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
     height: 48,
-    backgroundColor: "#E4E4E4",
+    backgroundColor: "#F1F1F1",
     flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    alignSelf: "center",
+    borderRadius: 10,
+    width: 282,
+    marginTop: 40
+  },
+  tabViewTwo: {
+    height: 48,
+    backgroundColor: "#F1F1F1",
+    flexDirection: "row",
+    justifyContent: "space-around",
     alignItems: "center",
     borderRadius: 10,
-    padding: 6,
-    marginVertical: 10
+    width: "100%",
+    marginTop: 40
   },
   tabItem: {
-    borderRadius: 10,
-    flex: 1,
-    height: "100%",
+    height: 37,
     justifyContent: "center",
     alignItems: "center",
-    flexDirection: "row"
-  },
-  selected: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
+    alignSelf: "center",
+    backgroundColor: "#FFF",
+    shadowColor: "gray",
     elevation: 10,
-    shadowOffset: {
-      width: 0,
-      height: 2
-    },
-    shadowOpacity: 0.2,
-    shadowRadius: 5
-  },
-  unSelected: {
-    backgroundColor: "#f1f1f1"
-  },
-  icon: {
-    width: 20,
-    height: 20,
-    resizeMode: "contain",
-    marginRight: 5
-  },
-  selectedIcon: {
-    tintColor: "#000"
-  },
-  unSelectedIcon: {
-    tintColor: "#7C7C7C"
-  }
-});
-
-const Input = props => {
-  return (
-    <View style={[inputStyles.inputContainer, props.containerStyle]}>
-      {props.text
-        ? (
-        <Text style={inputStyles.inputText}>{props.text}</Text>
-          )
-        : null}
-
-      <TextInput
-        style={[
-          inputStyles.input,
-          props.style,
-          props.textArea ? inputStyles.textArea : null
-        ]}
-        placeholder={props.placeholder ? props.placeholder : "Enter"}
-        value={props.value}
-        onChangeText={() => props.onChange()}
-        placeholderTextColor={
-          props.placeholderTextColor ? props.placeholderTextColor : "#9B9B9B"
-        }
-        editable={props.editable !== false}
-        autoCapitalize="none"
-        autoCorrect={false}
-        multiline={!!props.textArea}
-        backgroundColor={props.backgroundColor}
-        secureTextEntry={props.secureTextEntry}
-      />
-      {props.errorText
-        ? (
-        <Text style={inputStyles.error}>{props.errorText}</Text>
-          )
-        : null}
-      {props.icon
-        ? (
-        <Pressable
-          onPress={() => props.iconOnPress()}
-          style={inputStyles.iconWithText}>
-          <Image source={props.icon} style={inputStyles.icon} />
-        </Pressable>
-          )
-        : null}
-      <View style={styles.children}>{props.children}</View>
-    </View>
-  );
-};
-
-const inputStyles = StyleSheet.create({
-  inputContainer: {
-    flexDirection: "column",
-    justifyContent: "center"
-  },
-  inputText: {
-    fontSize: 14,
-    marginLeft: 20,
-    color: "#111112"
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
     borderRadius: 10,
-    padding: 10,
-    paddingLeft: 20,
-    marginVertical: 10,
-    width: "100%",
-    height: 50,
-    color: "#000"
+    color: "#000",
+    width: 81,
+    marginHorizontal: 10
   },
-  iconWithText: {
-    position: "absolute",
-    right: 30,
-    bottom: 25,
-    width: 20,
-    height: 20,
-    justifyContent: "center",
+  tabText: {
+    fontSize: 12,
+    color: "#7C7C7C",
+    textAlign: "center"
+  },
+  walletHeadings: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    zIndex: 1
+    paddingHorizontal: 32,
+    paddingTop: 40
   },
-  icon: {
-    width: "100%",
-    height: "100%",
-    resizeMode: "contain"
-  },
-  textArea: {
-    height: 150
-  },
-  children: {}
-});
-
-const Button = params => {
-  const backgroundColor = params.backgroundColor || "#000";
-  const textColor = params.textColor || "#fff";
-  const btnStyle = {
-    backgroundColor: backgroundColor,
-    borderColor: params.borderColor || backgroundColor,
-    borderWidth: 1
-  };
-  const btnText = {
-    color: textColor
-  };
-  return (
-    <View style={[buttonStyles.btnContainer, params.style]}>
-      <View style={!params.hideShadow ? buttonStyles.shadowContainer : null}>
-        <Pressable
-          style={[buttonStyles.btn, btnStyle]}
-          onPress={params.onPress}>
-          <Text style={[buttonStyles.btnText, btnText]}>
-            {params.buttonText}
-          </Text>
-          <View style={styles.childrenContainer}>{params.children}</View>
-        </Pressable>
-      </View>
-    </View>
-  );
-};
-
-const buttonStyles = StyleSheet.create({
-  btnContainer: {
-    justifyContent: "center"
-  },
-  shadowContainer: {
-    shadowColor: "rgba(0, 0, 0, 0.5)",
-    shadowOffset: {
-      width: 0,
-      height: 5
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    borderRadius: 10
-  },
-  btn: {
-    height: 50,
-    padding: 10,
-    paddingHorizontal: 25,
-    borderRadius: 10,
-    justifyContent: "center",
+  walletAreaSection: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-
-    flexDirection: "row"
+    marginHorizontal: 32,
+    paddingTop: 40
   },
-  btnText: {
-    color: "#fff",
+  mainHeading: {
     fontSize: 16,
-    fontWeight: "bold"
+    color: "#000000"
   },
-  childrenContainer: {
-    justifyContent: "center",
+  mainContainer: {
+    height: 266,
+    backgroundColor: "#F1F1F1",
+    borderRadius: 10,
+    marginHorizontal: 15,
+    marginTop: 20
+  },
+  innerView: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 18
+  },
+  innerHalfView: {
+    flexDirection: "row",
     alignItems: "center"
+  },
+  coinText: {
+    fontSize: 14,
+    color: "#000000",
+    marginLeft: 20,
+    marginBottom: 10
+  },
+  coinSubText: {
+    fontSize: 12,
+    color: "#ADB1B2",
+    marginLeft: 20
+  },
+  balanceText: {
+    fontSize: 18,
+    color: "#26292A",
+    marginBottom: 10
+  },
+  balanceAmountTextPositive: {
+    fontSize: 12,
+    color: "#12D790",
+    textAlign: "right"
+  },
+  balanceAmountTextNegative: {
+    fontSize: 12,
+    color: "#EA4335",
+    textAlign: "right"
+  },
+  borderWhite: {
+    height: 1,
+    marginHorizontal: 25,
+    borderWidth: 1,
+    borderColor: "#FFFFFF"
+  },
+  imageGraph: {
+    marginHorizontal: 15,
+    height: 226
+  },
+  graphImg: {
+    resizeMode: "contain",
+    width: "100%",
+    height: 226
+  },
+  indicatorsSection: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    height: 68,
+    justifyContent: "space-around",
+    paddingHorizontal: 10,
+    marginTop: -30
+  },
+  blueCircle: {
+    width: 12,
+    height: 12,
+    backgroundColor: "#3443CF",
+    borderRadius: 100 / 2,
+    marginTop: 4
+  },
+  greenCircle: {
+    width: 12,
+    height: 12,
+    backgroundColor: "#41C174",
+    borderRadius: 100 / 2,
+    marginTop: 4
+  },
+  redCircle: {
+    width: 12,
+    height: 12,
+    backgroundColor: "#F81506",
+    borderRadius: 100 / 2,
+    marginTop: 4
+  },
+  innerIndicator: {
+    flexDirection: "row"
+  },
+  tabParent: {
+    paddingHorizontal: 10,
+    marginTop: -30,
+    marginBottom: 10
   }
 });
+
+export default walletHomeScreen;
